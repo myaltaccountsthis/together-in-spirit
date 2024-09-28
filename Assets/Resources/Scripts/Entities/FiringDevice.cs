37af4302e5b2da567c52c78da4d5b8de5b851e1c
Sahasrad Chippa <52544077+MetaFlash@users.noreply.github.com>
2024-09-28 17:27:37 -0400

    Moving Saw, Crossbow, Projectiles + Arrow + Homing, Basic Spike

---
+++ Assets/Resources/Scripts/Entities/FiringDevice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FiringDevice : Entity
+{   
+    public Projectile projectilePrefab;
+    public Vector2 fixedDirection;
+    public bool isEnabled = true;
+    public float delay;
+    public bool isHoming;
+    public User homingTarget;
+    
+    
+    protected override void Awake()
+    {
+        StartCoroutine(firing());
+        fixedDirection = fixedDirection.normalized;
+    }
+
+    IEnumerator firing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            if (!enabled) continue;
+            projectilePrefab.isHoming = isHoming;
+            projectilePrefab.homingTarget = homingTarget;
+            projectilePrefab.direction = fixedDirection;
+            Projectile proj = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+        }
+    }
+
+    public void SetEnabled(bool enabled)
+    {
+        this.isEnabled = enabled;
+    }
+}
---
+++ Assets/Resources/Scripts/Entities/Saw.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class Saw : Killbrick
+{
+    public Transform leftPos;
+    public Transform rightPos;
+    public bool startRight = false;
+    public float oscillationTime;
+    public float rotationSize;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        transform.position = startRight ? rightPos.position : leftPos.position;
+        LeanTween
+            .move(gameObject, startRight ? leftPos : rightPos, oscillationTime)
+            .setEaseInOutSine()
+            .setLoopPingPong();
+
+        LeanTween.rotate(gameObject, rotationSize * (startRight ? Vector3.back : Vector3.forward), oscillationTime)
+            .setEaseInOutSine()
+            .setLoopPingPong();
+    }
+}
