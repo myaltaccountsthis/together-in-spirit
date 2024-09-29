8c458f9e360c906255465064bedfd4d9d50ce73a
Sahasrad Chippa <52544077+MetaFlash@users.noreply.github.com>
2024-09-29 03:20:48 -0400

    Room 1 Puzzle

--- Assets/Resources/Scripts/Entities/Note.cs
+++ Assets/Resources/Scripts/Entities/Note.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer.Internal;
+using UnityEngine;
 
 public class Note : Interactable
 {
-    // go
+    public string title;
     public string message;
 
     protected override void Awake()
@@ -14,6 +16,25 @@ public class Note : Interactable
 
     public override void Interact(User user)
     {
-        user.dialogueManager.Show(new("Old note", new string[] { message }));
+        // split message into a string array, basically i want as many sentences as possible in each before 150 chars is reached in one string, sentence ends with period
+        string current = "";
+        List<string> sentences = new List<string>();
+        foreach (string sentence in message.Split('.'))
+        {
+            string trimmed = sentence.Trim();
+            if (trimmed.Length == 0) continue;
+            bool empty = current.Length == 0;
+            if (empty || current.Length + trimmed.Length + 2 <= 150)
+            {
+                current += (empty  ? "" : " ") + trimmed + ".";
+            }
+            else
+            {
+                sentences.Add(current);
+                current = trimmed + ".";
+            }
+        }
+        sentences.Add(current);
+        user.dialogueManager.Show(new Dialogue(title, sentences.ToArray()));
     }
 }
